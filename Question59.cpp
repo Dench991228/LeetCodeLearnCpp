56ae5bc95f104eba18b82661a2e19abf95fe4779
Dench991228 <denghanzhi85139@163.com>
2021-03-16 23:05:15 +0800

    一道水水的签到题

---
+++ Question59.cpp
@@ -0,0 +1,109 @@
+#include <vector>
+#include <list>
+#include <iostream>
+
+using namespace std;
+class pos{
+public:
+    int r,c;
+public:
+    pos(){}
+    pos(int a, int b):r(a), c(b){}
+    void setPos(int a, int b){
+        r = a;
+        c = b;
+    }
+    void right(){
+        c++;
+    }
+    void left(){
+        c--;
+    }
+    void up(){
+        r--;
+    }
+    void down(){
+        r++;
+    }
+};
+class myIter{
+private:
+    int max_r, min_r, max_c, min_c;
+    pos current_position;
+    int former_direction;
+public:
+    myIter(int a, int b, int c, int d):min_r(a), max_r(b), min_c(c), max_c(d), former_direction(-1),current_position(0,-1){}
+    /**
+     * 一口气走到头，把路上的位置输出成列表，按照右、下、左、上的顺序
+    */
+    list<pos> move(){
+        list<pos> result;
+        former_direction += 1;
+        former_direction %= 4;
+        switch (former_direction){
+            case 0://一直往右
+                for(int i=current_position.c;i+1<max_c;i++){
+                    current_position.right();
+                    result.push_back(current_position);
+                }
+                max_c--;
+                break;
+            case 1://往下
+                for(int i=current_position.r;i+1<max_r;i++){
+                    current_position.down();
+                    result.push_back(current_position);
+                }
+                max_r--;
+                break;
+            case 2://往左
+                for(int i=current_position.c;i-1>min_c;i--){
+                    current_position.left();
+                    result.push_back(current_position);
+                }
+                min_c++;
+                break;
+            case 3://往上
+                for(int i=current_position.c;i-1>min_r;i--){
+                    current_position.up();
+                    result.push_back(current_position);
+                }
+                min_c++;
+                break;
+        }
+        return result;
+    }
+    bool movable(){
+        int temp = (former_direction+1)%4;
+        switch(temp){
+            case 0:
+                return (current_position.c+1<max_c)||(current_position.r+1<max_r);
+            case 1:
+                return (current_position.r+1<max_r)||(current_position.c-1>min_c);
+            case 2:
+                return (current_position.r+1<max_r)||(current_position.c-1>min_c);
+            case 3:
+                return (current_position.c+1<max_c)||(current_position.r-1>min_r);
+
+        }
+        return false;
+    }
+};
+class Solution{
+public:
+    vector<vector<int> > generateMatrix(int n) {
+        int height = n;
+        int width = n;
+        myIter it(0, height, 0, width);
+        vector<vector<int> > result;
+        result.assign(n, vector<int>(n));
+        int i=1;
+        while(it.movable()){
+            list<pos> line = it.move();
+            for(pos p:line){
+                result[p.r][p.c] = i;
+                i++;
+            }
+        }
+        return result;
+    }
+};
